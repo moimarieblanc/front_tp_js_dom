cfd3709c89558491c6d5c91e86a61595236fff19
moimarieblanc <moimarieblanc@gmail.com>
2020-09-09 15:42:02 +0200

    premier commit tp js dom

---
+++ src/dom.js
@@ -0,0 +1,49 @@
+// Changez la couleur de fond du body en #992323
+// Ne modifiez pas le code HTML de la page directement
+// votre code
+
+// Changez la couleur du texte du body en #fff
+// votre code
+
+// Supprimez la ligne suivante
+var workspace = null;
+// Récupérez l'élément div "workspace" par son id
+// Enregistrez-le dans une variable workspace
+// votre code
+
+// Affichez dans la console le nombre d'éléments enfants que contient
+// l'élément workspace
+// votre code
+
+// Récupérez le premier paragraphe contenu dans workspace
+// vous ne devez pas ajouter d'id
+// Supprimez la ligne suivante
+var p = null;
+// Affectez le résultat à la variable p
+// votre code
+console.log(p);
+
+// Ajoutez le code suivant : "<h3>Hello JS world!</h3>"
+// comme enfant de workspace
+// Cet élément doit être ajouté avant le premier paragraphe
+// contenu dans workspace
+// votre code
+
+const initTime = 10;
+
+// Complétez la fonction runChrono pour qu'elle affiche un décompte
+// de n à 0, dans une div (id: counter), placée sous la balise h3 précédemment ajoutée
+// Vous ne devez pas toucher au code HTML, et le h3 doit continuer à s'afficher
+function runChrono(/* votre code */) {
+  // votre code
+}
+
+console.log(runChrono(/* votre code */));
+
+// Ajoutez à l'élément workspace un bouton permettant de déclencher
+// le décompte lors d'un clic, avec le texte "Start countdown"
+// Le bouton doit être inséré après l'élément h3
+// Vous ne devez pas utiliser l'élément p pour insérer le bouton
+// Le décompte est réinitialisé lorsque l'utilisateur clique une
+// nouvelle fois sur le bouton
+// votre code
